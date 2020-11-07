9ebefe563761f5ccf65c000980dbd8b8431902eb
Andrii Tkach <papa.tkach@gmail.com>
2020-11-07 19:19:40 +0200

    microservices

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-import os
-import json
-import datetime
-from flask import Flask
-
-app = Flask(__name__)
-
-BASE_FOLDER = os.path.dirname(os.path.abspath(__file__))
-RESOURCE_DIR = os.path.join(BASE_FOLDER, "resources")
+import time
+from uuid import uuid4
+from storage import MongodbService
 
 
-@app.route('/')
-def hello_world():
-    with open(os.path.join(RESOURCE_DIR, "response.json")) as f:
-        return "%s - %s" % (json.loads(f.read()).get("payload"), datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
+storage = MongodbService.get_instance()
+
+for _ in range(5):
+    dto = {
+        "_id": str(uuid4()),
+        "payload": str(uuid4()),
+        "field2": str(int(time.time()))
+    }
+    storage.save_data(dto)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+for data in storage.get_data():
+    print(data)
